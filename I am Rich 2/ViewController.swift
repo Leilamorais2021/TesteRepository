0ffdedfb9f8c1efb0e2c258b972e128607dc4ff5
Leila Morais <leilasmorais@hotmail.com>
2021-04-26 12:42:29 -0400

    Initial Commit

---
+++ I am Rich 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich 2
+//
+//  Created by user195476 on 4/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
